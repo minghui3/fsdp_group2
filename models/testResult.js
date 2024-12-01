689030ac94c2bd29101abfa855d3258f1d0183bf
Inexpensive Hippo <s10261024@connect.np.edu.sg>
2024-12-01 21:01:58 +0800

    did not test schema

--- models/testResult.js
+++ models/testResult.js
@@ -1,10 +1,60 @@
 const mongoose = require("mongoose");
 
-const testResultSchema = new mongoose.Schema({
-    tests: {
-        type: [featureResultSchema],
+const stepMatchSchema = new mongoose.Schema({
+    location: {
+        type: String,
         required: true,
     },
+
+    // not present if step does not require arguements
+    arguments: [String], // ignore offset, keep value
+});
+
+const resultSchema = new mongoose.Schema({
+    status: { 
+        type: Boolean,
+        required: true,
+    },
+    duration: Number // ignore if step is in before/after instead of steps 
+})
+
+const stepResultSchema = new mongoose.Schema({
+    
+    result: {
+        type: resultSchema, // ignore duration for each step
+        required: true,
+    },
+    match: {
+        type: stepMatchSchema,
+        required: true,
+    },
+
+    // not present in before and after in scenarioResultSchema
+    keyword: String,
+    name: String,
+});
+
+const scenarioResultSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true,
+    },
+    keyword: {
+        type: String,
+        required: true,
+    },
+    steps: {
+        type: [stepResultSchema],
+        required: true,
+    },
+
+    // not present if type of scenario is Background
+    start_timestamp: Date,
+    name: String,
+
+    // optional if scenario is not Background, else not present.
+    before: [stepResultSchema],
+    after: [stepResultSchema],
 });
 
 const featureResultSchema = new mongoose.Schema({
@@ -30,52 +80,11 @@ const featureResultSchema = new mongoose.Schema({
     },
 });
 
-const scenarioResultSchema = new mongoose.Schema({
-    type: {
-        type: { type: String },
+const testResultSchema = new mongoose.Schema({
+    tests: {
+        type: [featureResultSchema],
         required: true,
     },
-    keyword: {
-        type: String,
-        required: true,
-    },
-    steps: {
-        type: [stepResultSchema],
-        required: true,
-    },
-
-    // not present if type of scenario is Background
-    start_timestamp: Date,
-    name: String,
-
-    // optional if scenario is not Background, else not present.
-    before: [stepResultSchema],
-    after: [stepResultSchema],
-});
-
-const stepResultSchema = new mongoose.Schema({
-    result: {
-        type: { duration: Number, status: Boolean },
-        required: true,
-    },
-    match: {
-        type: stepMatchSchema,
-        required: true,
-    },
-
-    // not present in before and after in scenarioResultSchema
-    keyword: String,
-    name: String,
-});
-
-const stepMatchSchema = new mongoose.Schema({
-    location: {
-        type: String,
-        required: true,
-    },
-
-    // not present if step does not require arguements
-    arguments: [String], // ignore offset, keep value
 });
 
 module.exports = testResultSchema;
